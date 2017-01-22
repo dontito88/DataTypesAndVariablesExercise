432fd95a8fccb77ad4834ee701b50b5a7091d586
TrifonEstov <don.tito.g@gmail.com>
2017-01-22 18:55:08 +0200

    added new program

---
+++ DataTypesExercises/PracticeIntNumbers/PracticeIntNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeIntNumbers
+{
+    class PracticeIntNumbers
+    {
+        static void Main(string[] args)
+        {
+            var num1 = double.Parse(Console.ReadLine());
+            var num2 = double.Parse(Console.ReadLine());
+            var num3 = double.Parse(Console.ReadLine());
+            var num4 = double.Parse(Console.ReadLine());
+            var num5 = double.Parse(Console.ReadLine());
+            var num6 = double.Parse(Console.ReadLine());
+            var num7 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+            Console.WriteLine(num4);
+            Console.WriteLine(num5);
+            Console.WriteLine(num6);
+            Console.WriteLine(num7);
+        }
+    }
+}
