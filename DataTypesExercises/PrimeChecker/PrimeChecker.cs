cbde4e8b841554b5137d2aba4abf62297da1d60b
TrifonEstov <don.tito.g@gmail.com>
2017-01-25 21:32:17 +0200

    New program added

---
+++ DataTypesExercises/PrimeChecker/PrimeChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeChecker
+{
+    class PrimeChecker
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            for (int number = 2; number <= input; number++)
+            {
+                bool checker = true;
+                for (int area = 2; area <= Math.Sqrt(number); area++)
+                {
+                    if (number % area == 0)
+                    {
+                        checker = false;
+                        break;
+                    }
+
+                }
+                Console.WriteLine($"{number} -> {checker}");
+            }
+        }
+    }
+}
