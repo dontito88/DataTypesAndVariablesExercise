18f0b4808379215f2a2547c2ba1e99a4812d1f99
TrifonEstov <don.tito.g@gmail.com>
2017-01-25 21:18:21 +0200

    New program added

---
+++ DataTypesExercises/IntToHexAndBinar/IntToHexAndBinar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntToHexAndBinar
+{
+    class IntToHexAndBinar
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string hex = Convert.ToString(input, 16);
+            string binary = Convert.ToString(input, 2);
+            Console.WriteLine(hex.ToUpper());
+            Console.WriteLine(binary.ToUpper());
+        }
+    }
+}
